5fd61624e960364c37640ad67c12593b7932b549
MrDavid <david.turaev@gmail.com>
2024-06-04 01:42:23 +0400

    Добавил пробелы

--- src/homework6/task2/Main.java
+++ src/homework6/task2/Main.java
@@ -5,17 +5,24 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         Scanner sc = new Scanner(System.in);
 
         String str = sc.nextLine();
 
         File file = new File("src/homework6/task2/tst.txt");
 
-        try (FileOutputStream fos = new FileOutputStream(file); FileInputStream fis = new FileInputStream(file)) {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             FileInputStream fis = new FileInputStream(file)) {
+
             fos.write(str.getBytes(StandardCharsets.UTF_8));
+
             int i;
+
             while ((i = fis.read()) != -1) {
+
                 if ((char) i == ' ') {
                     System.out.print('_');
                     continue;
@@ -23,6 +30,5 @@ public class Main {
                 System.out.print((char) i);
             }
         }
-
     }
 }
